8d01bff0280a15460cc82c1731b94cff1ae11f15
hdinjos <hdinjos@gmail.com>
2021-04-22 14:24:23 +0700

    post single file

--- index.ts
+++ index.ts
@@ -5,6 +5,8 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as Loki from 'lokijs';
 
+import {loadCollection} from './utils';
+
 
 const DB_NAME = 'db.json';
 const COLLECTION_NAME = 'images';
@@ -14,6 +16,18 @@ const db = new Loki(`${UPLOAD_PATH}/${DB_NAME}`, {persistenceMethod: 'fs'});
 
 
 const app = express();
+
+app.post('/profile', upload.single('avatar'), async (req, res) => {
+  try {
+    const col = await loadCollection(COLLECTION_NAME, db);
+    const data = col.insert(req.file);
+
+    db.saveDatabase();
+    res.send({id: data.$loki, filename: data.filename, originalName: data.originalname});
+  }catch (err){
+    res.sendStatus(404);
+  }
+})
 
 app.use(cors());
 app.listen(3000, ()=> {
